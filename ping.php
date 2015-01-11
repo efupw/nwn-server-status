2e47eba666fc0ffb3e3ea842c6f0b8762ee11d80
Mikkel Kjeldsen <commonquail@gmail.com>
2015-01-11 16:05:46 +0100

    Encapsulate in function

--- ping.php
+++ ping.php
@@ -1,28 +1,33 @@
 <?php
 require_once 'config.local.php';
 
-$name = 3;
-$current_players = 5;
-$max_players = 6;
-$description = 15;
-$fp = fsockopen("udp://{$hostname}", $port, $errno, $errstr, 2);
-if (!$fp) {
-  echo "Offline";
-} else {
-  $send = "\xFE\xFD\x00\xE9\x49\x04\x05\x14\x01\x0B\x01\x05\x08\x0A\x33\x34\x35\x13\x04\x36\x37\x38\x39\x14\x3A\x3B\x3C\x3D\x00\x00";
+function nwn_server_status($hostname, $port) {
+    $fp = fsockopen("udp://{$hostname}", $port, $errno, $errstr, 2);
+    if (!$fp) {
+        return "Offline";
+    }
 
-  fwrite($fp, $send);
-  stream_set_timeout($fp, 2);
-  $fresponse = fread($fp, 5000);
-  fclose($fp);
-  if ($fresponse) {
+    $send = "\xFE\xFD\x00\xE9\x49\x04\x05\x14\x01\x0B\x01\x05\x08\x0A\x33\x34\x35\x13\x04\x36\x37\x38\x39\x14\x3A\x3B\x3C\x3D\x00\x00";
+
+    fwrite($fp, $send);
+    stream_set_timeout($fp, 2);
+    $fresponse = fread($fp, 5000);
+    fclose($fp);
+    if (!$fresponse) {
+        return "Offline";
+    }
+
+    $name = 3;
+    $current_players = 5;
+    $max_players = 6;
+    $description = 15;
     $res = explode("\x00", $fresponse);
-    echo("Online ("
+    return "Online ("
         . $res[$current_players]
         . " / "
         . $res[$max_players]
-        . " players)");
-  } else {
-    echo("Offline");
-  }
+        . " players)";
 }
+
+$status = nwn_server_status($hostname, $port);
+echo $status;
